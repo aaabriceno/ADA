55bb9c481acc9d5251919ea872899f81b563433e
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-08-23 19:03:48 -0500

    Calculadora Practica

--- Semana_1/Calculadora_practica/Calculadora_practica.cpp
+++ Semana_1/Calculadora_practica/Calculadora_practica.cpp
@@ -1,19 +1,97 @@
-// Calculadora_practica.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
+/*
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+float Adicion(float a, float b) {
+    return a + b;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+float Sustraccion(float a, float b) {
+    return a - b;
+}
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+float Multiplicacion(float a, float b) {
+    return a * b;
+}
+
+float Division(float a, float b) {
+    if (b != 0)
+        return a / b;
+    else {
+        cout << "No se puedo una division por cero!" << endl;
+        return 0;
+    }
+}
+
+int main() {
+    float a, b, c;
+    int opt;
+
+    cout << "Ingresa dos numeros: ";
+    cin >> a >> b;
+
+    cout << "Ingresa la opcion (0-Adicion, 1-Sustraccion, 2-Multiplicacion, 3-Division): ";
+    cin >> opt;
+
+    switch (opt) {
+    case 0: c = Adicion(a, b); break;
+    case 1: c = Sustraccion(a, b); break;
+    case 2: c = Multiplicacion(a, b); break;
+    case 3: c = Division(a, b); break;
+    default: cout << "Opcion invalida" << endl; return 1;
+    }
+
+    cout << "Resultado: " << c << endl;
+    return 0;
+}
+*/
+
+
+#include <iostream>
+using namespace std;
+
+float Adicion(float a, float b) {
+    return a + b;
+}
+
+float Sustraccion(float a, float b) {
+    return a - b;
+}
+
+float Multiplicacion(float a, float b) {
+    return a * b;
+}
+
+float Division(float a, float b) {
+    if (b != 0)
+        return a / b;
+    else {
+        cout << "Error: Division por cero!" << endl;
+        return 0;
+    }
+}
+
+typedef float (*lpfnOperation)(float, float);
+
+int main() {
+    lpfnOperation vpf[4] = {&Adicion, &Sustraccion, &Multiplicacion, &Division};
+
+    float a, b, c;
+    int opt;
+
+    cout << "Ingresa dos numeros: ";
+    cin >> a >> b;
+
+    cout << "Ingresa la operacion (0-Adicion, 1-Sustraccion, 2-Multiplicacion, 3-Division): ";
+    cin >> opt;
+
+    if (opt >= 0 && opt < 4) {
+        c = (*vpf[opt])(a, b);
+        cout << "Resultado: " << c << endl;
+    } else {
+        cout << "Opcion invalida!" << endl;
+    }
+
+    return 0;
+}
+
