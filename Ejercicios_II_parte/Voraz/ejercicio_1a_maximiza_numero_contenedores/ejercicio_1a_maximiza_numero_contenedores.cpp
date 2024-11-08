f62c35ac9e87e10804fdd2036d7262297edcf6bb
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-11-08 17:04:39 -0500

    Ejercicio 1a maxima numero contenedores

---
+++ Ejercicios_II_parte/Voraz/ejercicio_1a_maximiza_numero_contenedores/ejercicio_1a_maximiza_numero_contenedores.cpp
@@ -0,0 +1,50 @@
+/*
+Se tiene un buque mercante cuya capacidad de carga es de k toneladas y un
+conjunto de contenedores c1, …, cn cuyos pesos respectivos son p1, …, pn
+(expresados también en toneladas). Teniendo en cuenta que la capacidad del
+buque es menor que la suma total de los pesos de los contenedores:
+a. Diseñe un algoritmo que maximice el número de contenedores cargados.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct Contenedor {
+    int id;
+    double peso;
+
+    Contenedor(int i, double p) : id(i), peso(p) {}
+};
+
+bool compararPorPeso(const Contenedor& a, const Contenedor& b) {
+    return a.peso < b.peso; // Ordenar de forma no decreciente (menor a mayor)
+}
+
+void maximizarContenedores(std::vector<Contenedor>& contenedores, double capacidadBuque) {
+    std::sort(contenedores.begin(), contenedores.end(), compararPorPeso);
+
+    double pesoCargado = 0.0;
+    int contenedoresCargados = 0;
+
+    for (const Contenedor& contenedor : contenedores) {
+        if (pesoCargado + contenedor.peso <= capacidadBuque) {
+            pesoCargado += contenedor.peso;
+            contenedoresCargados++;
+            std::cout << "Contenedor " << contenedor.id << " cargando:"<< contenedor.peso <<" toneladas" << std::endl;
+        }
+    }
+
+    std::cout << "Total de contenedores cargados: " << contenedoresCargados << std::endl;
+    std::cout << "Peso total cargado: " << pesoCargado << " toneladas." << std::endl;
+}
+
+int main() {
+    double capacidadBuque = 20.0;
+    std::vector<Contenedor> contenedores = { {1, 12.0}, {2, 2.5}, {3, 1.5}, {4, 14.0}, {5, 2.3}, {6, 4.6},{7,3.9},{8,1.2} ,{9,0.7} };
+
+    maximizarContenedores(contenedores, capacidadBuque);
+
+    return 0;
+}
+
