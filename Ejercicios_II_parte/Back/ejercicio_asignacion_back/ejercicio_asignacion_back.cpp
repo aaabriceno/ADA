ef119bd92e5006f0a6bfe409e2efb7d2674005ec
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-11-22 22:56:36 -0500

    Correxion Ejercicio de asginacion de tareas backtracking

--- Ejercicios_II_parte/Back/ejercicio_asignacion_back/ejercicio_asignacion_back.cpp
+++ Ejercicios_II_parte/Back/ejercicio_asignacion_back/ejercicio_asignacion_back.cpp
@@ -3,18 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-// Variables globales
-int n = 3; // Número de personas/tareas
-vector<vector<int>> B = {
-    {4, 9, 1},
-    {7, 2, 3},
-    {6, 3, 5}
-}; // Matriz de beneficios
-int voa = 0; // Mejor beneficio encontrado
-vector<int> mejor_asignacion; // Mejor asignación encontrada
-
-// Función para calcular el beneficio total de una asignación
-int calcularBeneficio(const vector<int>& asignacion) {
+int calcularBeneficio(const vector<int>& asignacion, const vector<vector<int>>& B, int n) {
     int beneficio = 0;
     for (int i = 0; i < n; ++i) {
         beneficio += B[i][asignacion[i]];
@@ -23,11 +12,11 @@ int calcularBeneficio(const vector<int>& asignacion) {
 }
 
 // Backtracking para encontrar la mejor asignación
-void resolverAsignacion(vector<int>& asignacion, vector<bool>& usado, int nivel) {
+void resolverAsignacion(vector<int>& asignacion, vector<bool>& usado, int nivel, int n, const vector<vector<int>>& B, int& mejor_beneficio, vector<int>& mejor_asignacion) {
     if (nivel == n) { // Si se asignaron todas las tareas
-        int beneficio = calcularBeneficio(asignacion);
-        if (beneficio > voa) {
-            voa = beneficio;
+        int beneficio = calcularBeneficio(asignacion, B, n);
+        if (beneficio > mejor_beneficio) {
+            mejor_beneficio = beneficio;
             mejor_asignacion = asignacion;
         }
         return;
@@ -37,14 +26,23 @@ void resolverAsignacion(vector<int>& asignacion, vector<bool>& usado, int nivel)
         if (!usado[tarea]) { // Si la tarea no ha sido asignada
             asignacion[nivel] = tarea;
             usado[tarea] = true;
-            resolverAsignacion(asignacion, usado, nivel + 1);
+            resolverAsignacion(asignacion, usado, nivel + 1, n, B, mejor_beneficio, mejor_asignacion);
             usado[tarea] = false; // Backtracking
         }
     }
 }
 
 int main() {
-    // Imprimir la matriz de beneficios
+    int n = 3;
+    vector<vector<int>> B = {
+        {4, 9, 1},
+        {7, 2, 3},
+        {6, 3, 5}
+    };
+
+    int mejor_beneficio = 0;
+    vector<int> mejor_asignacion;
+
     cout << "Matriz de beneficios:\n";
     for (const auto& fila : B) {
         for (int val : fila) {
@@ -54,18 +52,14 @@ int main() {
     }
     cout << "\n";
 
-    // Inicialización
-    vector<int> asignacion(n, -1); // Vector para almacenar la asignación actual
-    vector<bool> usado(n, false); // Vector para marcar tareas asignadas
+    vector<int> asignacion(n, -1);
+    vector<bool> usado(n, false);
 
-    // Resolver el problema de asignación
-    resolverAsignacion(asignacion, usado, 0);
+    resolverAsignacion(asignacion, usado, 0, n, B, mejor_beneficio, mejor_asignacion);
 
-    // Mostrar el resultado
-    cout << "Mejor beneficio: " << voa << "\n";
+    cout << "Mejor beneficio: " << mejor_beneficio << "\n";
     cout << "Matriz de asignaciones:\n";
 
-    // Construir la matriz de asignaciones a partir de la mejor asignación
     vector<vector<int>> matriz_sol(n, vector<int>(n, 0));
     for (int i = 0; i < n; ++i) {
         matriz_sol[i][mejor_asignacion[i]] = 1;
